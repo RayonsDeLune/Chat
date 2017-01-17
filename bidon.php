4afb52ff15e0d0a3ca38287a877b75338f1e025f
Formation <Formation@10.100.200.99>
2017-01-17 10:17:49 +0100

    Modifications pour tester la modif sous git ( fichier bidon ajouté, et ajout d'un espace dans chat.php )

---
+++ bidon.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+// juste pour tester les modifs avec git
+?>
+
--- chat.php
+++ chat.php
@@ -1,5 +1,4 @@
-<?php include_once("_config.php"); ?>
-<?php
+<?php include_once("_config.php"); 
 session_start();
 // récupère le pseudo en session
 $pseudo = $_SESSION["pseudo"];
